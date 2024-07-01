d8af1a122f0314d04fb9dc5c693a01a923ec08e5
Prateek Dahiya <100485095+PrateekDahiya@users.noreply.github.com>
2024-07-01 15:14:49 +0530

    added app

---
+++ videoquality.py
@@ -0,0 +1,75 @@
+from flask import Flask, request, jsonify
+import yt_dlp
+
+app = Flask(__name__)
+
+
+def get_video_qualities(video_url):
+    ydl_opts = {
+        'listformats': False,
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info_dict = ydl.extract_info(video_url, download=False)
+        formats = info_dict.get('formats', [])
+
+        video_quality_map = {}
+        best_audio = None
+        highest_bitrate = 0
+
+        for f in formats:
+            if f.get('ext') == 'webm' and f.get('vcodec') != 'none':
+                resolution = f.get('height')
+                if resolution is not None and resolution not in video_quality_map:
+                    video_quality_map[resolution] = f.get('url')
+            elif f.get('acodec') != 'none' and f.get('abr') is not None:
+                bitrate = f.get('abr')
+                if bitrate > highest_bitrate:
+                    highest_bitrate = bitrate
+                    best_audio = f.get('url')
+
+        video_quality_list = [{"resolution": res, "url": url} for res, url in video_quality_map.items()]
+        return video_quality_list, best_audio
+
+
+def get_video_url_by_quality(video_list, selected_quality):
+    if not video_list:
+        return None
+
+    selected_resolution = selected_quality.rstrip('p')  # Remove 'p' from resolution if present
+    for video in video_list:
+        resolution = str(video['resolution']).rstrip('p') if video['resolution'] else None
+        if resolution == selected_resolution:
+            return video['url']
+    return None
+
+
+@app.route('/get_video_url', methods=['GET'])
+def get_video_url():
+    video_id = request.args.get('video_id')
+    quality = request.args.get('quality')
+    if not video_id:
+        return jsonify({"error": "Video ID must be provided"}), 400
+
+    video_url = f'https://www.youtube.com/watch?v={video_id}'
+    video_qualities, best_audio_url = get_video_qualities(video_url)
+
+    if quality:
+        selected_video_url = get_video_url_by_quality(video_qualities, quality)
+        if selected_video_url:
+            return jsonify({
+                "selected_video_url": selected_video_url,
+                "best_audio_url": best_audio_url,
+                "video_quality_options": video_qualities
+            })
+        else:
+            return jsonify({"error": f"No URL found for {quality}"}), 404
+    else:
+        return jsonify({
+            "best_audio_url": best_audio_url,
+            "video_quality_options": video_qualities
+        })
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8111)
